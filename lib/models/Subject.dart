b91f8418d7493820fd29128160287d63ab50c7a5
darylldan <dccaponpon@up.edu.ph>
2023-08-15 20:36:40 +0800

    feat: added a Subject model

---
+++ lib/models/Subject.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:timetable/util/enums.dart';
+
+class Subject {
+  String subjectID;
+  String courseCode;
+  bool isLaboratory;
+  String? description;
+  String section;
+  String room;
+  String? instructor;
+  String termID;
+  List<Day> frequency;
+  String? notes;
+
+  DateTime startDate;
+  DateTime endDate;
+
+  Color color;
+
+  Subject({
+    required this.subjectID,
+    required this.courseCode,
+    required this.isLaboratory,
+    this.description,
+    required this.section,
+    required this.room,
+    this.instructor,
+    required this.termID,
+    required this.frequency,
+    this.notes,
+    required this.startDate,
+    required this.endDate,
+    required this.color,
+  });
+}
