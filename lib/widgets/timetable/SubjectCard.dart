b4ab06105d74ebe4b05fadeb5bb66698c06f1e5a
darylldan <dccaponpon@up.edu.ph>
2023-08-15 20:45:21 +0800

    feat: added a subject card with self positioning for timetable

---
+++ lib/widgets/timetable/SubjectCard.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import '../../utils/constants.dart' as Constants;
+import '../../models/Subject.dart';
+
+class SubjectCard extends StatelessWidget {
+  final Subject subject;
+
+  SubjectCard({super.key, required this.subject});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: _getPositionOffset(),
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(6)),
+            color: subject.color),
+        width: Constants.subjectCardWidth,
+        height: _computeCardHeight(),
+        child: Padding(
+          padding: const EdgeInsets.all(2),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  subject.courseCode,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 9),
+                ),
+              ),
+              FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  subject.section,
+                  style: TextStyle(fontSize: 9),
+                ),
+              ),
+              FittedBox(
+                clipBehavior: Clip.hardEdge,
+                fit: BoxFit.none,
+                child: Text(
+                  subject.room,
+                  style: TextStyle(fontSize: 7),
+                  overflow: TextOverflow.fade,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  num _getMinutesDuration() {
+    return subject.endDate.difference(subject.startDate).inMinutes;
+  }
+
+  double _computeCardHeight() {
+    var minutesDuration = _getMinutesDuration();
+    var hoursDuration = (minutesDuration / 60).floor();
+    var remainingMinutesPercentage = (minutesDuration % 60) / 60;
+
+    if (hoursDuration == 0) {
+      return (Constants.oneHourHeight * remainingMinutesPercentage);
+    }
+
+    return 54 * hoursDuration -
+        (hoursDuration - 1) +
+        (Constants.oneHourHeight * remainingMinutesPercentage);
+  }
+
+  double _getPositionOffset() {
+    var minutesDuration = _getMinutesDuration() % 60;
+    var hourOffset = subject.startDate.hour - 7;
+
+    if (subject.startDate.hour == 7) {
+      return 7.5;
+    }
+
+    return 7.5 +
+        (Constants.oneHourHeight * hourOffset - (hourOffset - 1)) -
+        1 +
+        (Constants.oneHourHeight * (minutesDuration / 60));
+  }
+}
