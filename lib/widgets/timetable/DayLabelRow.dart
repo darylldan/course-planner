e4c5dd8d653635c975f1b6d98f52fc2dce8e2da0
darylldan <dccaponpon@up.edu.ph>
2023-08-15 20:42:26 +0800

    feat: added a complete row of day labels widget

---
+++ lib/widgets/timetable/DayLabelRow.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../../utils/enums.dart';
+
+import 'DayLabel.dart';
+
+class DayLabelRow extends StatelessWidget {
+  const DayLabelRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SizedBox(width: 40),
+        DayLabel(day: Day.mon, isOpaque: true),
+        DayLabel(day: Day.tue, isOpaque: false),
+        DayLabel(day: Day.wed, isOpaque: true),
+        DayLabel(day: Day.thu, isOpaque: false),
+        DayLabel(day: Day.fri, isOpaque: true),
+        DayLabel(day: Day.sat, isOpaque: false),
+      ],
+    );
+  }
+}
