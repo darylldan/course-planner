94f7607d05464e1d826a17c935eb2cb7ce8b3120
darylldan <dccaponpon@up.edu.ph>
2023-08-15 20:37:07 +0800

    feat: added a Term model

---
+++ lib/models/Term.dart
@@ -0,0 +1,10 @@
+class Term {
+  String termID;
+  String semester;
+  String academicYear;
+
+  Term(
+      {required this.termID,
+      required this.semester,
+      required this.academicYear});
+}
