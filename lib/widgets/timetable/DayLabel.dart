4922f75f3781c98c8e5d45b44706a1c03bb38063
darylldan <dccaponpon@up.edu.ph>
2023-08-15 20:41:04 +0800

    feat: added label for day to be used in timetable

---
+++ lib/widgets/timetable/DayLabel.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import '../../utils/enums.dart';
+
+class DayLabel extends StatelessWidget {
+  var isOpaque;
+  Day day;
+
+  DayLabel({super.key, required this.day ,required this.isOpaque});
+
+  @override
+  Widget build(BuildContext context) {
+    if (isOpaque) {
+      return _opaqueLabel(context);
+    }
+
+    return _transparentLabel(context);
+  }
+
+  Widget _opaqueLabel(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surfaceVariant,
+          borderRadius: BorderRadius.all(Radius.circular(4))),
+      width: 53,
+      height: 17,
+      child: Center(
+        child: Text(
+          day.name.toUpperCase(),
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).colorScheme.onSurfaceVariant,
+              fontSize: 9),
+        ),
+      ),
+    );
+  }
+
+  Widget _transparentLabel(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+            width: 1.5,
+            color: Theme.of(context).colorScheme.onSurfaceVariant,
+            strokeAlign: BorderSide.strokeAlignInside
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(4))),
+      width: 53,
+      height: 17,
+      child: Center(
+        child: Text(
+          day.name.toUpperCase(),
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).colorScheme.onSurfaceVariant,
+              fontSize: 9),
+        ),
+      ),
+    );
+  }
+}
