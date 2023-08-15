db39ad8eaba87a51cba78cda12c9350f552255c9
darylldan <dccaponpon@up.edu.ph>
2023-08-15 20:35:35 +0800

    chore: minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,6 @@ class _HomeState extends State<Home> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    
-
     return Scaffold(
         appBar: AppBar(
           title: const Text("Material Theme Builder"),
@@ -42,14 +40,40 @@ class _HomeState extends State<Home> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                'Count: $count',
+              Card(
+                color: Theme.of(context).colorScheme.tertiaryContainer,
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    'Count: $count',
+                  ),
+                ),
               ),
             ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
             onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(Icons.info_outline_rounded),
+                          AlertDialog(
+                            backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
+                            title: Text("Hello world!!!"),
+                            actions: [
+                              TextButton(onPressed: () {}, child: Text("Hello!"))
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+
               setState(() {
                 count++;
               });
