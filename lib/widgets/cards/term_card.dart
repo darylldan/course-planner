e829ce8ca1de484808ea3a8e569f649851001efd
darylldan <dccaponpon@up.edu.ph>
2023-08-20 13:12:30 +0800

    feat: implemented the actions menu for the term card kebab

--- lib/widgets/cards/term_card.dart
+++ lib/widgets/cards/term_card.dart
@@ -13,84 +13,140 @@ class TermCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 6.0),
-      child: Material(
-        child: Ink(
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.surfaceVariant,
-            borderRadius: BorderRadius.circular(Constants.cardBorderRadius)),
-          child: InkWell(
-            onTap: onTap,
-            borderRadius: BorderRadius.circular(Constants.cardBorderRadius),
-            child: _termCardContainer(context),
+        padding: const EdgeInsets.symmetric(vertical: 6.0),
+        child: Material(
+          child: Ink(
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surfaceVariant,
+                borderRadius:
+                    BorderRadius.circular(Constants.cardBorderRadius)),
+            child: InkWell(
+              onTap: onTap,
+              borderRadius: BorderRadius.circular(Constants.cardBorderRadius),
+              child: _termCardContainer(context),
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 
   Widget _termCardContainer(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.symmetric(
-            horizontal: Constants.titleCardPaddingH, vertical: 12),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 2, right: 14),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Theme.of(context).colorScheme.onSurfaceVariant),
-                    width: 4,
-                    height: 50,
-                  ),
+      padding: const EdgeInsets.symmetric(
+          horizontal: Constants.titleCardPaddingH, vertical: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 2, right: 14),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Theme.of(context).colorScheme.onSurfaceVariant),
+                  width: 4,
+                  height: 50,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 275,
-                      child: Text(
-                        term.semester,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color:
-                                Theme.of(context).colorScheme.onSurfaceVariant),
-                      ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 275,
+                    child: Text(
+                      term.semester,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color:
+                              Theme.of(context).colorScheme.onSurfaceVariant),
                     ),
-                    SizedBox(
-                      width: 275,
-                      child: Text(
-                        term.academicYear,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                            fontSize: 13,
-                            fontWeight: FontWeight.w300,
-                            color:
-                                Theme.of(context).colorScheme.onSurfaceVariant),
-                      ),
-                    )
-                  ],
-                ),
-                IconButton(
-                  onPressed: () async {
-                    context.read<TermProvider>().deleteTerm(term);
-                  },
-                  icon: const Icon(Icons.more_vert_rounded),
-                )
-              ],
-            )
-          ],
-        ),
-      );
+                  ),
+                  SizedBox(
+                    width: 275,
+                    child: Text(
+                      term.academicYear,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w300,
+                          color:
+                              Theme.of(context).colorScheme.onSurfaceVariant),
+                    ),
+                  )
+                ],
+              ),
+              IconButton(
+                onPressed: () {
+                  _showActions(context);
+                },
+                icon: const Icon(Icons.more_vert_rounded),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 
   void onTap() {
     print("tapped!");
+  }
+
+  void _showActions(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return SizedBox(
+          height: 350,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: Constants.screenHorizontalPadding, vertical: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Center(
+                  child: Text(
+                    "Actions",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ListTile(
+                  title: const Text("Set as Current Term"),
+                  leading: const Icon(Icons.star_rounded),
+                  onTap: () {
+                    context.read<TermProvider>().setCurrentTerm(term);
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text("Current term was set."),
+                    ));
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: const Text("Edit Term"),
+                  leading: const Icon(Icons.edit_rounded),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: const Text("Delete Term"),
+                  leading: const Icon(Icons.delete_forever_rounded),
+                  onTap: () {
+                    context.read<TermProvider>().deleteTerm(term);
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text("Term Deleted."),
+                    ));
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
