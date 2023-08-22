f734c6d7da0fe4c144dcf8718f39d375cbc97063
darylldan <dccaponpon@up.edu.ph>
2023-08-22 14:30:07 +0800

    feat: added Day methods

--- lib/utils/enums.dart
+++ lib/utils/enums.dart
@@ -1,1 +1,27 @@
-enum Day { mon, tue, wed, thu, fri, sat}
+enum Day { mon, tue, wed, thu, fri, sat }
+
+extension DayMethods on Day {
+  static Day fromInt(int dayOfWeek) {
+    if (dayOfWeek >= 1 && dayOfWeek <= Day.values.length) {
+      return Day.values[dayOfWeek - 1];
+    }
+    return Day.values[0];
+  }
+
+  static String dayToString(Day day) {
+    switch (day) {
+      case Day.mon:
+        return "Monday";
+      case Day.tue:
+        return "Tuesday";
+      case Day.wed:
+        return "Wednesday";
+      case Day.thu:
+        return "Thursday";
+      case Day.fri:
+        return "Friday";
+      case Day.sat:
+        return "Saturday";
+    }
+  }
+}
