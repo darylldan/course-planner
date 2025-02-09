f03586493492ce5d2799715ae820f54f22c2ad1c
darylldan <dccaponpon@up.edu.ph>
2025-02-09 23:43:06 +0800

    feat: added startdate and enddate methods on addterm

--- lib/screens/terms_module/add_term.dart
+++ lib/screens/terms_module/add_term.dart
@@ -5,6 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../models/Term.dart';
 import '../../utils/constants.dart' as Constants;
+import 'package:intl/intl.dart';
+
+enum DatePickerType { start, end }
 
 class AddTerm extends StatefulWidget {
   const AddTerm({super.key});
@@ -15,8 +18,16 @@ class AddTerm extends StatefulWidget {
 
 class _AddTermState extends State<AddTerm> {
   final _screenTitle = "Create New Term";
+
   final TextEditingController _semesterCtrl = TextEditingController();
   final TextEditingController _acadYearCtrl = TextEditingController();
+
+  DateTime? _startDate;
+  DateTime? _endDate;
+
+  // Date validators
+  bool _isStarDateInvalid = false;
+  bool _isEndDateInvalid = false;
 
   final _formKey = GlobalKey<FormState>();
 
@@ -41,7 +52,8 @@ class _AddTermState extends State<AddTerm> {
                   child: Divider(),
                 ),
               ),
-              _buildForm(context)
+              _buildForm(context),
+              const SizedBox(height: 150)
             ],
           )),
     );
@@ -49,15 +61,23 @@ class _AddTermState extends State<AddTerm> {
 
   Widget _buildForm(BuildContext context) {
     return PopScope(
-      canPop: () {
-        return _acadYearCtrl.text.isEmpty && _semesterCtrl.text.isEmpty;
-      }(),
+      canPop: false,
       onPopInvokedWithResult: (didPop, result) async {
-        if (didPop) return;
+        if (!didPop) {
+          // Check if form is empty
+          bool isFormEmpty = _acadYearCtrl.text.isEmpty &&
+              _semesterCtrl.text.isEmpty &&
+              _startDate == null &&
+              _endDate == null;
 
-        var shouldExit = await _onWillPop(context);
-        if (shouldExit) {
-          if (context.mounted) Navigator.of(context).pop();
+          if (isFormEmpty) {
+            if (context.mounted) Navigator.of(context).pop();
+          } else {
+            var shouldExit = await _onWillPop(context);
+            if (shouldExit) {
+              if (context.mounted) Navigator.of(context).pop();
+            }
+          }
         }
       },
       child: Form(
@@ -101,6 +121,14 @@ class _AddTermState extends State<AddTerm> {
                 },
               ),
             ),
+            Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Column(
+                  children: [
+                    _datePicker(context, DatePickerType.start),
+                    _datePicker(context, DatePickerType.end)
+                  ],
+                )),
             const Padding(
               padding: EdgeInsets.symmetric(vertical: 10),
               child: Opacity(
@@ -121,12 +149,15 @@ class _AddTermState extends State<AddTerm> {
                         foregroundColor: WidgetStatePropertyAll(
                             Theme.of(context).colorScheme.onPrimaryContainer)),
                     onPressed: () {
-                      if (_formKey.currentState!.validate()) {
+                      bool isDatesValid = _validateDates();
+                      if (_formKey.currentState!.validate() && isDatesValid) {
                         _formKey.currentState?.save();
                         context.read<TermProvider>().addTerm(Term()
                           ..isCurrentTerm = false
                           ..semester = _semesterCtrl.text
-                          ..academicYear = _acadYearCtrl.text);
+                          ..academicYear = _acadYearCtrl.text
+                          ..startDate = _startDate!
+                          ..endDate = _endDate!);
 
                         if (context.mounted) {
                           ScaffoldMessenger.of(context).showSnackBar(
@@ -144,6 +175,74 @@ class _AddTermState extends State<AddTerm> {
         ),
       ),
     );
+  }
+
+  Widget _datePicker(BuildContext context, DatePickerType dateType) {
+    bool shouldShowError = (dateType == DatePickerType.start
+        ? (_isStarDateInvalid)
+        : (_isEndDateInvalid));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              dateType == DatePickerType.start ? "Start Date" : "End Date",
+            ),
+            TextButton(
+              onPressed: dateType == DatePickerType.end && _startDate == null
+                  ? null
+                  : () => _pickDateModal(context, dateType),
+              child: Text((dateType == DatePickerType.start
+                  ? (_startDate == null
+                      ? "Select Date"
+                      : DateFormat("MMM d, yyyy").format(_startDate!))
+                  : (_endDate == null
+                      ? "Select Date"
+                      : DateFormat("MMM d, yyyy").format(_endDate!)))),
+            )
+          ],
+        ),
+        if (shouldShowError)
+          Text(
+            "Please select a start date.",
+            style: TextStyle(
+                color: Theme.of(context).colorScheme.error, fontSize: 12),
+          ),
+      ],
+    );
+  }
+
+  void _pickDateModal(BuildContext context, DatePickerType dateType) async {
+    DateTime? returnDate = await showDatePicker(
+      currentDate: dateType == DatePickerType.start
+          ? _startDate ?? DateTime.now()
+          : _endDate ?? _startDate!.add(Duration(days: 2)),
+      context: context,
+      firstDate: dateType == DatePickerType.end
+          ? _startDate!.add(Duration(days: 1))
+          : DateTime(1900),
+      lastDate: DateTime(9999),
+    );
+
+    if (returnDate != null) {
+      setState(() {
+        if (dateType == DatePickerType.start) {
+          _startDate = returnDate;
+        } else {
+          _endDate = returnDate;
+        }
+      });
+    }
+  }
+
+  bool _validateDates() {
+    _isStarDateInvalid = _startDate == null;
+    _isEndDateInvalid = _endDate == null;
+
+    setState(() {});
+    return !(_isStarDateInvalid && _isEndDateInvalid);
   }
 
   Future<bool> _onWillPop(BuildContext context) async {
