bb6b92756304fef86692c56dbc7801830d9a926c
darylldan <dccaponpon@up.edu.ph>
2025-02-10 01:13:23 +0800

    fix: removed color_schemes

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 import 'package:course_planner/providers/subject_provider.dart';
 import 'package:course_planner/providers/term_provider.dart';
 import 'package:course_planner/screens/overview_module/overview.dart';
+import 'package:course_planner/styles/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import './styles/color_schemes.g.dart';
 
 void main() {
   runApp(MultiProvider(
@@ -23,18 +23,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final materialTheme = MaterialTheme(TextTheme(
+      displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      displayMedium: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+      displaySmall: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      // Add more text styles as needed
+    ));
     return MaterialApp(
       title: 'Course Planner',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: lightColorScheme,
-        fontFamily: (Platform.isAndroid) ? "Inter" : null
-      ),
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        colorScheme: darkColorScheme,
-        fontFamily: (Platform.isAndroid) ? "Inter" : null
-      ),
+      theme: materialTheme.light(),
+      darkTheme: materialTheme.dark(),
+      themeMode: ThemeMode.system,
       home: const Overview(),
     );
   }
