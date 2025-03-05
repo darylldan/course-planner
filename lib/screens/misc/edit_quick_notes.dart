5f2c030a40aafdc7a2c9940d5b5475ad22958591
darylldan <dccaponpon@up.edu.ph>
2025-03-05 16:09:35 +0800

    feat: modularized the quick notes widget

---
+++ lib/screens/misc/edit_quick_notes.dart
@@ -0,0 +1,198 @@
+import 'dart:async';
+
+import 'package:course_planner/models/Building.dart';
+import 'package:course_planner/models/Room.dart';
+import 'package:course_planner/providers/building_provider.dart';
+import 'package:course_planner/providers/room_provider.dart';
+import 'package:course_planner/providers/subject_provider.dart';
+import 'package:course_planner/widgets/elements/title_text.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/Subject.dart';
+import '../../utils/constants.dart' as C;
+
+/*
+ * Custom note editor that takes up the whole screen, like the Notes app from iOS
+ */
+
+class EditQuickNotes extends StatefulWidget {
+  final String? notes;
+  final int id;
+  final String type;
+  final String name;
+  final ValueChanged<String?> onNotesChanged;
+
+  const EditQuickNotes(
+      {super.key,
+      required this.notes,
+      required this.onNotesChanged,
+      required this.id,
+      required this.name,
+      required this.type});
+
+  @override
+  State<EditQuickNotes> createState() => _EditQuickNotesState();
+}
+
+class _EditQuickNotesState extends State<EditQuickNotes> {
+  final _screenTitle = "Edit Notes";
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _notesCtrl = TextEditingController();
+
+  Timer? _saveTimer;
+
+  void _setNotes(BuildContext context) {
+    _saveTimer?.cancel();
+
+    _saveTimer = Timer(const Duration(seconds: 2), () {
+      widget.onNotesChanged(_notesCtrl.text);
+      _saveNotes(context);
+    });
+  }
+
+  void _saveNotes(BuildContext context) {
+    if (widget.type == "building") {
+      Building bldg =
+          context.read<BuildingProvider>().getBuildingByID(widget.id);
+      bldg.notes = _notesCtrl.text;
+
+      context.read<BuildingProvider>().editBuilding(bldg);
+
+      return;
+    }
+
+    if (widget.type == "subject") {
+      Subject subj = context.read<SubjectProvider>().getSubjectByID(widget.id);
+      subj.notes = _notesCtrl.text;
+
+      context.read<SubjectProvider>().editSubject(subj);
+
+      return;
+    }
+
+    if (widget.type == "room") {
+      Room room = context.read<RoomProvider>().getRoomByID(widget.id);
+      room.notes = _notesCtrl.text;
+
+      context.read<RoomProvider>().editRoom(room);
+
+      return;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _notesCtrl.text = widget.notes ?? "";
+  }
+
+  @override
+  void dispose() {
+    _saveTimer?.cancel();
+    _notesCtrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Text(
+                widget.name,
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 16,
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:
+              const EdgeInsets.symmetric(horizontal: C.screenHorizontalPadding),
+          child: _buildNotesEditor(context),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNotesEditor(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TitleText(title: _screenTitle),
+          Column(
+            children: [_buildForm(context)],
+          ),
+          SizedBox(
+            height: 100,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildForm(BuildContext context) {
+    return PopScope(
+      canPop: true,
+      onPopInvokedWithResult: (didPop, result) async {
+        widget.onNotesChanged(_notesCtrl.text); // Save before exiting
+        _saveNotes(context);
+
+        if (!didPop) {
+          Navigator.of(context).pop();
+        }
+      },
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              maxLines: null,
+              autofocus: true,
+              controller: _notesCtrl,
+              keyboardType: TextInputType.multiline,
+              decoration: const InputDecoration.collapsed(
+                  hintText: "Enter notes here..."),
+              style: TextStyle(fontSize: 16),
+              onChanged: (String val) => _setNotes(context),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<bool> _onWillPop(BuildContext context) async {
+    return (await showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text("Discard changes?"),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(true),
+                    child: const Text('Discard'),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(false),
+                    child: const Text('Cancel'),
+                  )
+                ],
+              );
+            })) ??
+        false;
+  }
+}
---
+++ lib/widgets/cards/quick_notes_card.dart
@@ -0,0 +1,127 @@
+import 'package:course_planner/screens/misc/edit_quick_notes.dart';
+import 'package:flutter/material.dart';
+import '../../utils/constants.dart' as C;
+
+class QuickNotesCard extends StatefulWidget {
+  String? notes;
+  int id;
+  String type;
+  String name;
+
+  QuickNotesCard(
+      {super.key, required this.notes, required this.id, required this.type, required this.name});
+
+  @override
+  State<QuickNotesCard> createState() => _QuickNotesCardState();
+}
+
+class _QuickNotesCardState extends State<QuickNotesCard> {
+  void onNotesChanged(String? newNotes) {
+    setState(() {
+      widget.notes = newNotes;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Ink(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(C.cardBorderRadius),
+          color: Theme.of(context).colorScheme.onInverseSurface,
+        ),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => EditQuickNotes(
+                        notes: widget.notes,
+                        onNotesChanged: onNotesChanged,
+                        id: widget.id,
+                        type: widget.type, name: widget.name,)));
+          },
+          borderRadius: BorderRadius.circular(C.cardBorderRadius),
+          child: _notes(context),
+        ),
+      ),
+    );
+  }
+
+  Widget _notes(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+          vertical: C.titleCardPaddingV, horizontal: C.titleCardPaddingV),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child: Icon(
+                      Icons.format_list_bulleted_rounded,
+                      size: C.cardIconSize,
+                      color: Theme.of(context).colorScheme.onSurfaceVariant,
+                    ),
+                  ),
+                  Text(
+                    "Notes",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        color: Theme.of(context).colorScheme.onSurfaceVariant,
+                        fontSize: C.titleCardHeaderFontSize),
+                  ),
+                ],
+              ),
+              Text(
+                "Tap to Edit",
+                style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                    fontSize: C.titleCardHeaderFontSize),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          _notesContainer(context)
+        ],
+      ),
+    );
+  }
+
+  Widget _notesContainer(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(
+          vertical: C.titleCardPaddingV, horizontal: C.titleCardPaddingV),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(C.cardBorderRadius),
+        color: Theme.of(context).colorScheme.onSurfaceVariant,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Text(
+              (widget.notes == null || widget.notes!.isEmpty)
+                  ? "No notes."
+                  : widget.notes!,
+              style: TextStyle(
+                  fontSize: 14,
+                  color: Theme.of(context).colorScheme.onInverseSurface),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
