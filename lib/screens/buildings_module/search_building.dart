4a0b72e07b18e1db8b5429b9616e901ec644eae3
darylldan <dccaponpon@up.edu.ph>
2025-03-25 20:41:40 +0800

    feat: finished search building

--- lib/screens/buildings_module/search_building.dart
+++ lib/screens/buildings_module/search_building.dart
@@ -1,9 +1,15 @@
 import 'package:course_planner/models/Building.dart';
 import 'package:course_planner/models/Room.dart';
+import 'package:course_planner/models/Subject.dart';
+import 'package:course_planner/models/Term.dart';
 import 'package:course_planner/providers/building_provider.dart';
 import 'package:course_planner/providers/room_provider.dart';
+import 'package:course_planner/providers/subject_provider.dart';
+import 'package:course_planner/providers/term_provider.dart';
 import 'package:course_planner/widgets/cards/building_card.dart';
+import 'package:course_planner/widgets/cards/error_card_no_action.dart';
 import 'package:course_planner/widgets/cards/info_card.dart';
+import 'package:course_planner/widgets/cards/room_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -26,15 +32,9 @@ class _SearchBuildingState extends State<SearchBuilding> {
   late List<Building> _buildings;
   late List<Room> _rooms;
 
-  late bool _unassignedFlag;
-  late ResultFilter _resultFilter;
-
   @override
   void initState() {
     super.initState();
-
-    _unassignedFlag = false;
-    _resultFilter = ResultFilter.all;
     _buildings = [];
     _rooms = [];
   }
@@ -45,7 +45,7 @@ class _SearchBuildingState extends State<SearchBuilding> {
     _buildings = context.read<BuildingProvider>().building;
 
     // No rooms or buildings
-    if ((_rooms.isEmpty && _buildings.isEmpty) && _isFilterActive()) {
+    if ((_rooms.isEmpty && _buildings.isEmpty)) {
       return Scaffold(
         appBar: AppBar(),
         body: SingleChildScrollView(
@@ -63,14 +63,78 @@ class _SearchBuildingState extends State<SearchBuilding> {
       );
     }
 
-    return Scaffold(
-      appBar: _searchBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding:
-              const EdgeInsets.symmetric(horizontal: C.screenHorizontalPadding),
-          child: _buildResults(context),
-        ),
+    Term? currentTerm = context.read<TermProvider>().currentTerm;
+    List<Subject> courses = [];
+
+    if (currentTerm != null) {
+      courses = context
+          .read<SubjectProvider>()
+          .getSubjectsByTerm(currentTerm.id!)
+          .where((c) => c.courseCode
+              .toLowerCase()
+              .contains(_searchValue.text.toLowerCase()))
+          .toList();
+    }
+
+    List<Room> roomResults = _rooms.where((r) {
+      return r.roomName
+              .toLowerCase()
+              .contains(_searchValue.text.toLowerCase().trim()) ||
+          courses
+              .where((c) => c.locationType == "room")
+              .toList()
+              .any((s) => s.locationID == r.id);
+    }).toList();
+
+    List<Building> buildingResults = _buildings.where((b) {
+      return b.buildingName
+              .toLowerCase()
+              .contains(_searchValue.text.toLowerCase().trim()) ||
+          courses
+              .where((c) => c.locationType == "building")
+              .toList()
+              .any((s) => s.locationID == b.id) ||
+          roomResults.any((r) => r.buildingId == b.id);
+    }).toList();
+
+    List<Padding> buildingResultsCard = buildingResults
+        .map((b) => Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: BuildingCard(bldg: b),
+            ))
+        .toList();
+    List<Padding> roomResultsCard = roomResults
+        .map((r) => Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: RoomCard(room: r),
+            ))
+        .toList();
+
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: _searchBar(),
+        body: _searchValue.text.trim().isEmpty
+            ? Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: C.screenHorizontalPadding),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    InfoCard(
+                        content:
+                            "Start by typing the building name, room name, or course code above.")
+                  ],
+                ),
+              )
+            : TabBarView(
+                children: [
+                  _buildResultCards(context, buildingResultsCard),
+                  _buildResultCards(context, roomResultsCard)
+                ],
+              ),
       ),
     );
   }
@@ -83,7 +147,9 @@ class _SearchBuildingState extends State<SearchBuilding> {
           decoration: InputDecoration(
             suffixIcon: IconButton(
               onPressed: () {
-                _searchValue.clear();
+                setState(() {
+                  _searchValue.clear();
+                });
               },
               icon: const Icon(Icons.clear_rounded),
             ),
@@ -94,104 +160,54 @@ class _SearchBuildingState extends State<SearchBuilding> {
           },
         ),
       ),
-      actions: [
-        _isFilterActive()
-            ? Badge(
-                child: IconButton(
-                  onPressed: () {
-                    _showFilterPanel();
-                  },
-                  icon: const Icon(Icons.filter_alt_rounded),
+      bottom: _searchValue.text.trim().isEmpty
+          ? null
+          : TabBar(
+              dividerColor: Colors.transparent,
+              tabs: [
+                Tab(
+                  text: "Buildings",
+                  icon: Icon(Icons.business),
                 ),
-              )
-            : IconButton(
-                onPressed: () {
-                  _showFilterPanel();
-                },
-                icon: const Icon(Icons.filter_alt_rounded)),
-        const SizedBox(
-          width: 20,
-        )
-      ],
+                Tab(
+                  text: "Rooms",
+                  icon: Icon(Icons.meeting_room),
+                )
+              ],
+            ),
     );
   }
 
-  Widget _buildResults(BuildContext context) {
-    if (_searchValue.text.trim().isEmpty && !_isFilterActive()) {
-      return Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          InfoCard(
-              content:
-                  "Start by typing the building or room name above. Click the filter button to enhance your search.")
-        ],
-      );
-    }
-
-    List<Room> roomResults = _rooms.where((r) {
-      return r.roomName
-          .toLowerCase()
-          .contains(_searchValue.text.toLowerCase().trim());
-    }).toList();
-
-    List<Building> buildingResults = _buildings.where((b) {
-      return b.buildingName
-          .toLowerCase()
-          .contains(_searchValue.text.toLowerCase().trim());
-    }).toList();
-
-    // List<Subject> results = _subjects.where(
-    //   (s) {
-    //     return s.termID == _currentTerm!.id! &&
-    //         s.courseCode
-    //             .toLowerCase()
-    //             .contains(_searchValue.text.toLowerCase().trim());
-    //   },
-    // ).toList();
-
-    if (_unassignedFlag) {
-      roomResults = _rooms.where((r) => r.buildingId == -1).toList();
-    }
-
-    List<BuildingCard> buildingResultsCard =
-        buildingResults.map((b) => BuildingCard(bldg: b)).toList();
-    // List<RoomCard> roomResultsCard =
-    //     roomResults.map((r) => RoomCard(room: r)).toList();
-
-    // To-do: build the results with the resulttype filter
-    if (true) {
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: _resultHeader(),
-          ),
-          // ...resultsCard,
-          const Padding(
-            padding: EdgeInsets.only(top: 8),
-            child: Opacity(
-              opacity: 0.5,
-              child: Divider(),
+  Widget _buildResultCards(BuildContext context, List<Widget> results) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(horizontal: C.screenHorizontalPadding),
+      child: results.isEmpty
+          ? Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: ErrorCardNoAction(
+                  title: "SEARCH RESULTS", content: "No results found."))
+          : Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: _resultHeader(),
+                ),
+                ...results,
+                Center(
+                  child: Text(
+                    "${results.length} ${results.length == 1 ? "Result" : "Results"}",
+                    style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.onInverseSurface),
+                  ),
+                ),
+                const SizedBox(
+                  height: 120,
+                )
+              ],
             ),
-          ),
-          // Center(
-          //   child: Text(
-          //     "${resultsCard.length} ${resultsCard.length == 1 ? "Subject" : "Subjects"}",
-          //     style: TextStyle(
-          //         fontSize: 12,
-          //         fontWeight: FontWeight.bold,
-          //         color: Theme.of(context).colorScheme.onInverseSurface),
-          //   ),
-          // ),
-          const SizedBox(
-            height: 120,
-          )
-        ],
-      );
-    }
+    );
   }
 
   Widget _resultHeader() {
@@ -222,53 +238,5 @@ class _SearchBuildingState extends State<SearchBuilding> {
         ),
       ],
     );
-  }
-
-  void _showFilterPanel() {
-    showModalBottomSheet(
-      context: context,
-      builder: (context) {
-        return StatefulBuilder(builder: ((context, setModalState) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(
-                horizontal: C.screenHorizontalPadding, vertical: 24),
-            child: SizedBox(
-              height: 500,
-              width: double.infinity,
-              child: Column(
-                children: [
-                  const Center(
-                    child: Text(
-                      "Filters",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
-                    ),
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setModalState(() {
-                          _unassignedFlag = false;
-                          _resultFilter = ResultFilter.all;
-                        });
-                      },
-                      child: const Text(
-                        "Clear",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          );
-        }));
-      },
-    );
-  }
-
-  bool _isFilterActive() {
-    return _unassignedFlag || _resultFilter != ResultFilter.all;
   }
 }
